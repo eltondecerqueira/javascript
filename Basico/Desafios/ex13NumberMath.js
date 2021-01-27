69d417202a4f007905bc12a30711c9d675acf19c
Elton Cerqueira <eltoncerqueira@protonmail.com>
2021-01-27 17:54:13 +0000

    atividade completa

--- Basico/Desafios/ex13NumberMath.js
+++ Basico/Desafios/ex13NumberMath.js
@@ -1,4 +1,11 @@
 const numero = Number(prompt("Qual o seu número ?"));
 const numeroTitulo = document.getElementById('numeroTitulo');
+const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
+texto.innerHTML += `<p>A raiz quadrada de ${numero} é: ${(numero ** 0.5).toFixed(2)} </p>`;
+texto.innerHTML += `<p>${numero} é um inteiro ? ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)} </p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)} </p>`;
+texto.innerHTML += `<p>É NaN?  ${Number.isNaN(numero)} </p>`;
