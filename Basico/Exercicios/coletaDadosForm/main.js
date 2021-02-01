d92f4905b1b2ecea5227e8ce65cd89d4b89b4834
Elton Cerqueira <eltoncerqueira@protonmail.com>
2021-02-01 19:50:40 +0000

    editando objetos

--- Basico/Exercicios/coletaDadosForm/main.js
+++ Basico/Exercicios/coletaDadosForm/main.js
@@ -25,7 +25,7 @@ function controlaFormulario(){
         pessoas.push(criaGrupo)
 
         
-        resultado.innerHTML += `${criaGrupo.nome criaGrupo.sobrenome criaGrupo.peso criaGrupo.altura}<br/>`;
+        resultado.innerHTML += `${criaGrupo.nome, criaGrupo.sobrenome, criaGrupo.peso, criaGrupo.altura}<br/>`;
         
     }
 
